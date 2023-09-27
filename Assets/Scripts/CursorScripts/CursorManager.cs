21137358cf4cda7ce2c0439b76e048a3db0efa75
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2023-09-26 20:38:00 -0500

    cleaned up code
    - removed logs

--- Assets/Scripts/CursorScripts/CursorManager.cs
+++ Assets/Scripts/CursorScripts/CursorManager.cs
@@ -13,35 +13,29 @@ public class CursorManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     public void OnInteractiveItemCursorEnter()
     {
         // change to the circle cursor
-        Debug.Log("--> OnInteractiveItemCursorEnter() just fired!");
         Cursor.SetCursor(cursorCircleTransparentWhite, Vector2.zero, CursorMode.Auto);
     }
 
     public void OnInteractiveItemCursorLeave()
     {
         // change back to plus sign cursor
-        Debug.Log("--> OnInteractiveItemCursorLeave() just fired!");
         Cursor.SetCursor(cursorPlusSignTransparentWhite, Vector2.zero, CursorMode.Auto);
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("OnCollisionEnter2D just fired");
+        Debug.Log("OnCollisionEnter2D");
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-
-
+        Debug.Log("OnPointerEnter");
         Cursor.SetCursor(cursorCircleTransparentWhite, Vector2.zero, CursorMode.Auto);
-        //Debug.Log("onpointerenter");
-        Debug.Log("OnPointerEnter gameObject: ", this.gameObject);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        Debug.Log("onpointerExit");
         Cursor.SetCursor(cursorPlusSignTransparentWhite, Vector2.zero, CursorMode.Auto);
-        //Debug.Log("onpointerExit");
-        Debug.Log("onpointerExit GameObject: ", gameObject);
     }
 }
