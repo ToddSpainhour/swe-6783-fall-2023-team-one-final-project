e8e69a4977154e966ba53c55018dce3b9609f6a1
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2023-09-30 12:48:17 -0500

    cleaned up file
    - removed logs

--- Assets/Scripts/CursorScripts/CursorManager.cs
+++ Assets/Scripts/CursorScripts/CursorManager.cs
@@ -10,13 +10,11 @@ public class CursorManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     public void OnPointerEnter(PointerEventData eventData)
     {
         Cursor.SetCursor(cursorCircleTransparentWhite, Vector2.zero, CursorMode.Auto);
-        Debug.Log("OnPointerEnter in CursorManager");
     }
 
 
     public void OnPointerExit(PointerEventData eventData)
     {
         Cursor.SetCursor(cursorPlusSignTransparentWhite, Vector2.zero, CursorMode.Auto);
-        Debug.Log("onPointerExit in CursorManager");
     }
 }
