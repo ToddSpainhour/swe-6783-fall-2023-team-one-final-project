67a7146bdb85b838e0b7fc31030068e1762a750b
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2023-11-10 16:02:56 -0600

    key pad lights turn green is correct value is entered in correct position
    - key pad light turns green if correct value is entered in correct position

--- Assets/Scripts/SceneScripts/MuseumScripts/MuseumKeypadScript.cs
+++ Assets/Scripts/SceneScripts/MuseumScripts/MuseumKeypadScript.cs
@@ -10,35 +10,31 @@ public class MuseumKeypadScript : MonoBehaviour
     static string[] KeypadCode = { "4", "3", "1", "1", "2" };
     //static List<string> userEnteredKeyPadCodeSequence = new List<string> { "0", "0", "0", "0", "0" };
     static string[] userEnteredKeyPadCodeSequence = { "0", "0", "0", "0", "0" };
-
+    public GameObject keyPadLight1;
+    public GameObject keyPadLight2;
+    public GameObject keyPadLight3;
+    public GameObject keyPadLight4;
+    public GameObject keyPadLight5;
 
     private int amountOfNumbersEntered = 0;
     private string buttonValue;
     private string theSequenceYouEnteredSoFar = "";
 
-    void Start()
-    {
-          
-    }
-
     public void EnterKeypadButtonValue()
     {
         buttonValue = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Text>().text;
-
-
-
         userEnteredKeyPadCodeSequence[amountOfNumbersEntered] = buttonValue;
 
         for (int i = 0; i < userEnteredKeyPadCodeSequence.Length; i++)
         {
-            theSequenceYouEnteredSoFar += userEnteredKeyPadCodeSequence[i];
-            
+            theSequenceYouEnteredSoFar += userEnteredKeyPadCodeSequence[i]; 
         }
 
-        Debug.Log("theSequenceYouEnteredSoFar: " + theSequenceYouEnteredSoFar);
+        //Debug.Log("theSequenceYouEnteredSoFar: " + theSequenceYouEnteredSoFar);
 
         if (amountOfNumbersEntered == 4)
         {
+            ChangeKeypadLightColor();
             Debug.Log("theSequenceYouEnteredSoFar: " + theSequenceYouEnteredSoFar);
             CheckIfCorrectCodeWasEntered();
             amountOfNumbersEntered = 0;
@@ -48,22 +44,13 @@ public class MuseumKeypadScript : MonoBehaviour
         {
             userEnteredKeyPadCodeSequence[amountOfNumbersEntered] = buttonValue;
             theSequenceYouEnteredSoFar = "";
-
-            /*
-            for (int i = 0; i < userEnteredKeyPadCodeSequence.Length; i++)
-            {
-                theSequenceYouEnteredSoFar += userEnteredKeyPadCodeSequence[i];
-            }
-            */
+            ChangeKeypadLightColor();
             amountOfNumbersEntered++;
-            //Debug.Log("theSequenceYouEnteredSoFar: " + theSequenceYouEnteredSoFar);
         }
     }
 
     private void CheckIfCorrectCodeWasEntered()
     {
-        // loop through both keypad code array and user entered array if any are wrong bail out
-
         for(int i = 0; i < userEnteredKeyPadCodeSequence.Length; i++)
         {
             if (userEnteredKeyPadCodeSequence[i] != KeypadCode[i])
@@ -83,4 +70,35 @@ public class MuseumKeypadScript : MonoBehaviour
         }
     }
 
+    private void ChangeKeypadLightColor()
+    {
+        Debug.Log("userEnteredKeyPadCodeSequence[amountOfNumbersEntered] " + userEnteredKeyPadCodeSequence[amountOfNumbersEntered]);
+        Debug.Log("KeypadCode[amountOfNumbersEntered] " + KeypadCode[amountOfNumbersEntered]);
+        // if the number entered equal the number at the same index, turn the light green
+        if (userEnteredKeyPadCodeSequence[amountOfNumbersEntered] == KeypadCode[amountOfNumbersEntered])
+        {
+            switch (amountOfNumbersEntered)
+            {
+                // case is the index position
+                case 0:
+                    keyPadLight1.GetComponent<Renderer>().material.color = Color.green;
+                    break;
+                case 1:
+                    keyPadLight2.GetComponent<Renderer>().material.color = Color.green;
+                    break;
+                case 2:
+                    keyPadLight3.GetComponent<Renderer>().material.color = Color.green;
+                    break;
+                case 3:
+                    keyPadLight4.GetComponent<Renderer>().material.color = Color.green;
+                    break;
+                case 4:
+                    keyPadLight5.GetComponent<Renderer>().material.color = Color.green;
+                    break;
+                default:
+                    Debug.Log("inside deafult case statement");
+                    break;
+            }
+        }
+    }
 }
