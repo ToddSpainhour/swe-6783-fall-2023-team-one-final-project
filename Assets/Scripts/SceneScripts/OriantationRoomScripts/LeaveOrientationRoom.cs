051e15776834a9bf538ae705c90d2da7315550bf
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2023-11-11 10:16:24 -0600

    Orientation Room now leads to Museum
    - exiting the Orientation Room now leads to the Museum Room
    - cleaned up file

--- Assets/Scripts/SceneScripts/OriantationRoomScripts/LeaveOrientationRoom.cs
+++ Assets/Scripts/SceneScripts/OriantationRoomScripts/LeaveOrientationRoom.cs
@@ -5,20 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class LeaveOrientationRoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnMouseDown()
     {
-        SceneManager.LoadScene("ToBeContinued");
+        SceneManager.LoadScene("MuseumRoom");
     }
 }
