1913b627d7e1d6619bcb99e61214dee6fd1f923e
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2023-11-11 09:56:42 -0600

    exit hotspot now leads to next scene
    - clicking the exposed exit now advances to the next scene

--- Assets/Scripts/SceneScripts/MuseumScripts/AdvanceThroughMuseum.cs
+++ Assets/Scripts/SceneScripts/MuseumScripts/AdvanceThroughMuseum.cs
@@ -6,11 +6,13 @@ public class AdvanceThroughMuseum : MonoBehaviour
 {
     public GameObject paintingOnHiddenDoor;
     public GameObject hiddenDoorOpen;
+    public GameObject exit;
 
     public void OpenHiddenDoor()
     {
         paintingOnHiddenDoor.SetActive(false);
         hiddenDoorOpen.SetActive(true);
+        exit.SetActive(true);
     }
 
 }
---
+++ Assets/Scripts/SceneScripts/MuseumScripts/LeaveMuseumRoom.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LeaveMuseumRoom : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("ToBeContinued");
+    }
+}
